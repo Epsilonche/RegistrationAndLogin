c3e369236517588731ab402c7ea210d257b31fa8
mklei42s <P_2wgWip>
2021-05-21 12:11:18 +0200

    edit: michael

--- src/main/java/org/hbrs/se2/project/hellocar/entities/Company.java
+++ src/main/java/org/hbrs/se2/project/hellocar/entities/Company.java
@@ -31,23 +31,140 @@ public class Company {
 
     @Id
     @GeneratedValue
+
     @Column(name = "Company_id")
-    public int getId() {
+    public int getCompany_id() {
         return Company_id;
     }
-
-    public void setId(int id) {
+    public void setCompany_id(int id) {
         this.Company_id = id;
     }
 
     @Basic
-    @Column(name = "date_of_birth")
-    public LocalDate getDateOfBirth() {
-        return dateOfBirth;
+    @Column(name = "Company_branch_id")
+    public LocalDate getEstablishment_date() {
+        return Establishment_date;
     }
 
-    public void setDateOfBirth(LocalDate dateOfBirth) {
-        this.dateOfBirth = dateOfBirth;
+    public void setEstablishment_date(LocalDate Establishment_date) {
+        this.Establishment_date = Establishment_date;
+    }
+
+    @Column(name = "Company_id")
+    public void setCompany_branch_id(int company_branch_id) {
+        Company_branch_id = company_branch_id;
+    }
+
+    public int getPostal_Code() {
+        return Postal_Code;
+    }
+
+    public int getVAT_identification_number() {
+        return VAT_identification_number;
+    }
+
+    public String getHouse_Number() {
+        return House_Number;
+    }
+
+    public String getCompany_Name() {
+        return Company_Name;
+    }
+
+    public String getStreet() {
+        return Street;
+    }
+
+    public String getCity() {
+        return City;
+    }
+
+    public String getCountry() {
+        return Country;
+    }
+
+    public String getSecurity_answer() {
+        return Security_answer;
+    }
+
+    public String getHomepage() {
+        return Homepage;
+    }
+
+    public String getHeadquarters() {
+        return Headquarters;
+    }
+
+    public int getAmount_of_employees() {
+        return Amount_of_employees;
+    }
+
+    public int getSecurity_answer_id() {
+        return security_answer_id;
+    }
+
+    public String getCompany_description() {
+        return Company_description;
+    }
+
+    public int getTurnover() {
+        return Turnover;
+    }
+
+    public void setPostal_Code(int postal_Code) {
+        Postal_Code = postal_Code;
+    }
+
+    public void setVAT_identification_number(int VAT_identification_number) {
+        this.VAT_identification_number = VAT_identification_number;
+    }
+
+    public void setHouse_Number(String house_Number) {
+        House_Number = house_Number;
+    }
+
+    public void setCompany_Name(String company_Name) {
+        Company_Name = company_Name;
+    }
+
+    public void setStreet(String street) {
+        Street = street;
+    }
+
+    public void setCity(String city) {
+        City = city;
+    }
+
+    public void setCountry(String country) {
+        Country = country;
+    }
+
+    public void setSecurity_answer(String security_answer) {
+        Security_answer = security_answer;
+    }
+
+    public void setHomepage(String homepage) {
+        Homepage = homepage;
+    }
+
+    public void setHeadquarters(String headquarters) {
+        Headquarters = headquarters;
+    }
+
+    public void setAmount_of_employees(int amount_of_employees) {
+        Amount_of_employees = amount_of_employees;
+    }
+
+    public void setSecurity_answer_id(int security_answer_id) {
+        this.security_answer_id = security_answer_id;
+    }
+
+    public void setCompany_description(String company_description) {
+        Company_description = company_description;
+    }
+
+    public void setTurnover(int turnover) {
+        Turnover = turnover;
     }
 
     @Basic
@@ -58,6 +175,10 @@ public class Company {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public int getCompany_branch_id() {
+        return Company_branch_id;
     }
 
     @Basic
