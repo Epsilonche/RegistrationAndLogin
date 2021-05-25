dc56b7b074bd5bc598b02539b8f99f2287ddc3e6
SH2796 <79216642+SH2796@users.noreply.github.com>
2021-05-25 12:30:00 +0200

    /profile als Profilseite. Bei Login weiterleitung zum jwlg Profil

--- src/main/java/org/hbrs/se2/project/hellocar/util/Globals.java
+++ src/main/java/org/hbrs/se2/project/hellocar/util/Globals.java
@@ -10,7 +10,7 @@ public class Globals {
         public static final String ENTER_CAR = "enter";
 
         public static final String LOGIN_VIEW = "login";
-        public static final String MAIN_VIEW = "";
+        public static final String PROFILE_VIEW = "profile";
     }
 
     public static class Roles {
