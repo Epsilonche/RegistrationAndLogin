c9975be3780ae198928d31b6a884d920b7bf9fca
SH2796 <79216642+sh2796@users.noreply.github.com>
2021-05-31 11:59:58 +0200

    LoginView mit Weiterleitung zur Registrierungsseite. ToDo: Navigation bei erfolgreichen Login zur Profilseite(Zeile 92) muss noch upgedatet werden.

--- src/main/java/org/hbrs/se2/project/collhbrs/views/MainView.java
+++ src/main/java/org/hbrs/se2/project/collhbrs/views/MainView.java
@@ -1,11 +1,17 @@
 package org.hbrs.se2.project.collhbrs.views;
 
+import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.dialog.Dialog;
+import com.vaadin.flow.component.html.H3;
 import com.vaadin.flow.component.login.LoginForm;
-import com.vaadin.flow.component.notification.Notification;
+//import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouteAlias;
 import org.hbrs.se2.project.collhbrs.control.LoginControl;
@@ -16,21 +22,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * View zur Darstellung der Startseite. Diese zeigt dem Benutzer ein Login-Formular an.
- * ToDo: Integration einer Seite zur Registrierung von Benutzern
- */
-//@Route(value = "" )
+ **/
 
-@Route(value = "login" )
+
+@Route(value = "" )
 @RouteAlias(value = "login")
+@PageTitle("Login")
 public class MainView extends VerticalLayout {
 
     @Autowired
     private LoginControl loginControl;
 
+    private Button register = new Button("Registrieren");
+
     public MainView() {
         setSizeFull();
-        LoginForm component = new LoginForm();
+        add(createTitle());
 
+        register.addClickListener(e-> {
+            UI.getCurrent().navigate(Globals.Pages.REGISTRATION_VIEW);});
+
+        LoginForm component = new LoginForm();
         component.addLoginListener(e -> {
 
             boolean isAuthenticated = false;
@@ -46,31 +58,38 @@ public class MainView extends VerticalLayout {
             }
             if (isAuthenticated) {
                 grabAndSetUserIntoSession();
-                Notification.show("login successful.");//To test if the Login works without a MainPage
-                //navigateToMainPage(); //Use when MainPage is implemented
+                navigateToMainPage();
 
             } else {
-                // Kann noch optimiert werden
                 component.setError(true);
             }
         });
 
         add(component);
+        add(createButtonLayout());
         this.setAlignItems( Alignment.CENTER );
     }
 
+    private Component createButtonLayout() {
+        HorizontalLayout buttonLayout = new HorizontalLayout();
+        buttonLayout.addClassName("button-layout");
+        register.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        buttonLayout.add(register);
+        return buttonLayout;
+    }
+
+    private Component createTitle() {
+        return new H3(" Willkommen auf der Kollaborationsplattform Coll@HBRS!");
+    }
     private void grabAndSetUserIntoSession() {
         UserDTO userDTO = loginControl.getCurrentUser();
-        UI.getCurrent().getSession().setAttribute( Globals.CURRENT_USER, userDTO );
+        UI.getCurrent().getSession().setAttribute( Globals.CURRENT_USER , userDTO );
     }
 
 
     private void navigateToMainPage() {
-        // Navigation zur Startseite, hier auf die Teil-Komponente Show-Cars.
-        // Die anzuzeigende Teil-Komponente kann man noch individualisieren, je nach Rolle,
-        // die ein Benutzer besitzt
-
-        //UI.getCurrent().navigate(Globals.Pages.SHOW_CARS); muss ersetzt werden
+        // Navigation zur Startseite, hier die jeweilige Profilseite, die noch eingebunden werden muss
+        UI.getCurrent().navigate(Globals.Pages.PROFILE_VIEW);
 
     }
 }
