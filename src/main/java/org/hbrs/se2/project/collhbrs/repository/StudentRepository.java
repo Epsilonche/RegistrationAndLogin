fc8e3452d0c99e36fe1eaa0373c2b76d5566af9e
Othman Moukhli <othman.moukhli@smail.inf.h-brs.de>
2021-06-06 17:36:16 +0200

    Repository Student Company

---
+++ src/main/java/org/hbrs/se2/project/collhbrs/repository/StudentRepository.java
@@ -0,0 +1,10 @@
+package org.hbrs.se2.project.collhbrs.repository;
+
+import org.hbrs.se2.project.collhbrs.entities.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student,Integer> {
+
+}
