165337cea390bbc435333b42ee5e35e0e6a007c3
ayoub bakkar <ayoub.bakkar@smail.inf.h-brs.de>
2021-06-06 18:19:34 +0200

    student Factory

--- src/main/java/org/hbrs/se2/project/collhbrs/repository/StudentRepository.java
+++ src/main/java/org/hbrs/se2/project/collhbrs/repository/StudentRepository.java
@@ -1,8 +1,13 @@
 package org.hbrs.se2.project.collhbrs.repository;
 
+import org.hbrs.se2.project.collhbrs.dtos.UserDTO;
 import org.hbrs.se2.project.collhbrs.entities.Student;
+import org.hbrs.se2.project.collhbrs.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student,Integer> {
