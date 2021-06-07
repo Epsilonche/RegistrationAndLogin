407ebff317c2fe42505b5f0e260ee3e7d9a86d99
slind12s <sarah.linden@gmx.net>
2021-06-07 21:38:52 +0200

    Hinzufügen des Binders zum auslesen der Daten

--- src/main/java/org/hbrs/se2/project/collhbrs/control/ProfileManager.java
+++ src/main/java/org/hbrs/se2/project/collhbrs/control/ProfileManager.java
@@ -1,8 +1,15 @@
 package org.hbrs.se2.project.collhbrs.control;
 
+import com.vaadin.flow.component.UI;
 import org.hbrs.se2.project.collhbrs.control.factories.UserFactory;
 import org.hbrs.se2.project.collhbrs.dtos.CompanyDTO;
 import org.hbrs.se2.project.collhbrs.dtos.StudentDTO;
+import org.hbrs.se2.project.collhbrs.dtos.UserDTO;
+import org.hbrs.se2.project.collhbrs.dtos.impl.ProfDTOImpl;
+import org.hbrs.se2.project.collhbrs.entities.User;
+import org.hbrs.se2.project.collhbrs.repository.UserRepository;
+import org.hbrs.se2.project.collhbrs.util.Globals;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -10,6 +17,8 @@ public class ProfileManager {
     private boolean profile_created = false;
 
     UserFactory userFactory = new UserFactory();
+    @Autowired
+    private UserRepository userRepository;
 
     public boolean checkIfProfileIsCreated(){
         return profile_created;// TODO : must check if database contains a Student or Company profile assigned to a User ID instead of using the variable
@@ -20,6 +29,7 @@ public class ProfileManager {
     public boolean isCompany(){
         return true;//TODO has to be implemented
     }
+
 
     public void createStudentProfile(StudentDTO studentDTO){
         userFactory.createStudent(studentDTO);
@@ -32,6 +42,34 @@ public class ProfileManager {
         //TODO : use the repository to save the entity
         profile_created = true;
     }
+    // Methode zum löschen eines Users
+    // Achtung mögliche Exception ergänzen
+    // Vergleich zwischen currentUser und dem Binder-element
+    public boolean deleteUser(UserDTO userDTO) {
+        UserDTO current_user  = (UserDTO) UI.getCurrent().getSession().getAttribute(Globals.CURRENT_USER);
+        //Eingabe nicht erfolgreich
+        //Eingabe erfolgreich
+        // -> Löschen des Users aus der Datenbank
+        if(userDTO.getUsername().equals(current_user.getUsername())
+                && userDTO.getPassword().equals(current_user.getPassword())) {
+            userRepository.deleteById(current_user.getUserId());
+            // delete in Student/Company tables too ? what about related tables? Skills, Branches, Vacancies...
+
+            return true;
+        }
+        else return false;
+    }
+    public void updateUser(ProfDTOImpl neu, UserDTO akt) {
+
+        User usertoupdate = userRepository.findUserByUserId(akt.getUserId());
+        usertoupdate.setFirstName(neu.getFirstName());
+        usertoupdate.setLastName(neu.getLastName());
+        usertoupdate.seteMail(neu.geteMail());
+
+        this.userRepository.save(usertoupdate);
+    }
+
+
 
 
 
--- src/main/java/org/hbrs/se2/project/collhbrs/views/DeleteProfileView.java
+++ src/main/java/org/hbrs/se2/project/collhbrs/views/DeleteProfileView.java
@@ -1,16 +1,23 @@
 package org.hbrs.se2.project.collhbrs.views;
 
 import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.H3;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.textfield.PasswordField;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.component.textfield.TextFieldVariant;
+import com.vaadin.flow.data.binder.Binder;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouteAlias;
+import org.hbrs.se2.project.collhbrs.control.ProfileManager;
+import org.hbrs.se2.project.collhbrs.dtos.impl.UserDTOImpl;
 
 
 @Route(value = "delete")
@@ -18,33 +25,53 @@ import com.vaadin.flow.router.RouteAlias;
 @CssImport("./styles/views/entercar/enter-car-view.css")
 public class DeleteProfileView extends Div {
 
+
+    private PasswordField password = new PasswordField("Passwort");
+    private TextField username = new TextField("Username");
+
     private com.vaadin.flow.component.button.Button back = new com.vaadin.flow.component.button.Button("Back");
     private com.vaadin.flow.component.button.Button delete = new com.vaadin.flow.component.button.Button("Delete");
 
-    public DeleteProfileView() {
-        addClassName("delete-profile-view");
+    private Binder<UserDTOImpl> binder = new Binder(UserDTOImpl.class);
 
-        add(createNameField());
-        add(createPasswordField());
+    public DeleteProfileView(ProfileManager profileManager) {
+        addClassName("person-form-view");
+        add(createTitle());
+        add(createFormLayout());
         add(createButtonLayout());
 
+        binder.bindInstanceFields(this);
+        clearForm();
+
+
+        delete.addClickListener(e -> {
+
+            profileManager.deleteUser(binder.getBean());
+            Notification.show("Profil gelöscht");
+            UI.getCurrent().navigate(RegistrationView.class);
+        });
+        back.addClickListener(e ->{
+            UI.getCurrent().navigate(ProfileView.class);
+
+        });
+
+    }
+    private void clearForm() {
+        binder.setBean(new UserDTOImpl());
     }
 
-    private TextField createNameField() {
-        //TextField to enter the Username
-        TextField usernameField = new TextField("Enter your Name");
-        //usernameField.addThemeVariants(TextFieldVariant.LUMO_ALIGN_CENTER);
-        return usernameField;
+    private Component createTitle() {
+
+        return new H3("Profil löschen");
     }
 
-    private PasswordField createPasswordField() {
-        // add password field
-        PasswordField passwordfield = new PasswordField();
-        passwordfield.setLabel("Password");
-        passwordfield.setPlaceholder("Enter password");
-        passwordfield.setValue("secret1");
-        return passwordfield;
+    private Component createFormLayout() {
+        FormLayout formLayout = new FormLayout();
+        formLayout.add(username, password);
+        return formLayout;
     }
+
+
 
     private Component createButtonLayout() {
         HorizontalLayout buttonLayout = new HorizontalLayout();
@@ -57,5 +84,4 @@ public class DeleteProfileView extends Div {
 
     // need DialogField -> "your account was deleted"
     // Exception for wrong input
-
 }
