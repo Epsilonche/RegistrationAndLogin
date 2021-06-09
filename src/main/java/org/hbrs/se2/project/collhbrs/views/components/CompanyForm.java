16d1106d81293439eb8df214c15a958f72d0edb4
ayoub bakkar <ayoub.bakkar@smail.inf.h-brs.de>
2021-06-09 14:17:18 +0200

    companyForm Button save

--- src/main/java/org/hbrs/se2/project/collhbrs/views/components/CompanyForm.java
+++ src/main/java/org/hbrs/se2/project/collhbrs/views/components/CompanyForm.java
@@ -1,8 +1,13 @@
 package org.hbrs.se2.project.collhbrs.views.components;
 
+import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.binder.Binder;
+import org.hbrs.se2.project.collhbrs.dtos.impl.CompanyDTOImpl;
 
 public class CompanyForm extends FormLayout {
     private TextField branch = new TextField("branch");
@@ -12,13 +17,35 @@ public class CompanyForm extends FormLayout {
     private TextField description= new TextField("description");
 
     private Button save = new Button("Profil erstellen");
+    private Binder<CompanyDTOImpl> companyBinder = new Binder(CompanyDTOImpl.class);
+
+
+
 
     public CompanyForm() {
         add(branch,
             title,
             role,
             company,
-            description
+            description,
+                createButtonLayout()
         );
     }
+
+    private Component createButtonLayout() {
+        HorizontalLayout buttonLayout = new HorizontalLayout();
+        buttonLayout.addClassName("button-layout");
+        save.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        buttonLayout.add(save);
+        save.addClickListener( click -> { createCompany(companyBinder.getBean());
+
+        });
+        return buttonLayout;
+    }
+
+    private void createCompany(CompanyDTOImpl bean) {
+        System.out.print("COMPANY CREATED ");
+    }
+
+
 }
