6667fc609c793b4286357a4bed885cbc1e75541b
Othman Moukhli <othman.moukhli@smail.inf.h-brs.de>
2021-07-02 06:11:47 +0200

    security issue sonarqube RegistrationControl

--- src/main/java/org/hbrs/se2/project/collhbrs/views/RegistrationView.java
+++ src/main/java/org/hbrs/se2/project/collhbrs/views/RegistrationView.java
@@ -3,7 +3,6 @@ package org.hbrs.se2.project.collhbrs.views;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
-import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H3;
@@ -13,35 +12,12 @@ import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.radiobutton.RadioButtonGroup;
 import com.vaadin.flow.component.textfield.PasswordField;
 import com.vaadin.flow.component.textfield.TextField;
-import com.vaadin.flow.component.upload.Upload;
-import com.vaadin.flow.component.upload.receivers.MemoryBuffer;
 import com.vaadin.flow.data.binder.Binder;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import org.hbrs.se2.project.collhbrs.datatypes.RegistrationResult;
 import org.hbrs.se2.project.collhbrs.control.RegistrationControl;
 import org.hbrs.se2.project.collhbrs.dtos.impl.UserDTOImpl;
-
-
-import com.vaadin.flow.component.HasComponents;
-import com.vaadin.flow.component.HtmlComponent;
-import com.vaadin.flow.component.Tag;
-
-import com.vaadin.flow.internal.MessageDigestUtil;
-
-import com.vaadin.flow.server.StreamResource;
-
-import javax.imageio.ImageIO;
-import javax.imageio.ImageReader;
-import javax.imageio.stream.ImageInputStream;
-
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Iterator;
-
-import org.apache.commons.io.IOUtils;
-import org.hbrs.se2.project.collhbrs.views.components.ImageUpload;
 
 
 @Route(value = "register")
@@ -54,8 +30,6 @@ public class RegistrationView extends Div{
     private TextField username = new TextField("Benutzername");
     private PasswordField password = new PasswordField("Passwort"); //hidden password
     private RadioButtonGroup<String> user_typ = new RadioButtonGroup<>();
-
-    private Image img = new Image();
 
     private Button signUp = new Button("registrieren");
     private Binder<UserDTOImpl> binder = new Binder(UserDTOImpl.class);
@@ -114,9 +88,6 @@ public class RegistrationView extends Div{
         return buttonLayout;
     }
 
-    /* Upload Image Code copied from :
-    *  Credits : https://github.com/vaadin/vaadin-upload-flow/blob/master/vaadin-upload-flow-demo/src/main/java/com/vaadin/flow/component/upload/demo/UploadView.java
-    *
-    */
+
 
 }
